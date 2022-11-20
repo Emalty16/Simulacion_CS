38609d6bf512cebec110d9931718bb152a03fefe
Emalty16 <borgevinicio@gmail.com>
2022-11-19 19:28:39 -0600

    Creacion del caso de simulacion

---
+++ src/main/java/sc303/simulacion_cs/Simulacion_CS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package sc303.simulacion_cs;
+
+/**
+ *
+ * @author usuario
+ */
+public class Simulacion_CS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
